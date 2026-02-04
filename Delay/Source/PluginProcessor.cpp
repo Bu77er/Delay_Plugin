e8414c419aafb32eccdec905aeaa4de8257dcb0f
Glenn <m1e2e3e4p5@gmail.com>
2026-02-03 22:20:06 -0500

    Initial commit

---
+++ Delay/Source/PluginProcessor.cpp
@@ -0,0 +1,186 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+DelayAudioProcessor::DelayAudioProcessor()
+     : AudioProcessor (BusesProperties()
+                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)),
+                        params(apvts)
+{
+}
+
+DelayAudioProcessor::~DelayAudioProcessor()
+{
+}
+
+//==============================================================================
+const juce::String DelayAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool DelayAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool DelayAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool DelayAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double DelayAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int DelayAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int DelayAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void DelayAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String DelayAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void DelayAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
+//==============================================================================
+void DelayAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    params.prepareToPlay(sampleRate);
+    params.reset();
+
+}
+
+void DelayAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool DelayAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+    return layouts.getMainOutputChannelSet() == juce::AudioChannelSet::stereo();
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    // Some plugin hosts, such as certain GarageBand versions, will only
+    // load plugins that support stereo bus layouts.
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void DelayAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,[[maybe_unused]] juce::MidiBuffer& midiMessages)
+{
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+    
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
+    params.update();
+    float* channelDataL = buffer.getWritePointer(0);
+    float* channelDataR = buffer.getWritePointer(1);
+
+    for (int sample = 0; sample < buffer.getNumSamples(); sample++)
+    {
+        params.smoothen();
+        channelDataL[sample] *= params.gain;
+        channelDataR[sample] *= params.gain;
+    }
+}
+
+//==============================================================================
+bool DelayAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor* DelayAudioProcessor::createEditor()
+{
+    return new DelayAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void DelayAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
+    copyXmlToBinary(*apvts.copyState().createXml(), destData);
+}
+
+void DelayAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    std::unique_ptr<juce::XmlElement> xml(getXmlFromBinary(data, sizeInBytes));
+    if (xml.get() != nullptr && xml->hasTagName(apvts.state.getType()))
+        apvts.replaceState(juce::ValueTree::fromXml(*xml));
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new DelayAudioProcessor();
+}
+
+juce::AudioProcessorValueTreeState::ParameterLayout Parameters::createParameterLayout()
+{
+    juce::AudioProcessorValueTreeState::ParameterLayout layout;
+    layout.add(std::make_unique<juce::AudioParameterFloat>(gainParamID, "Output Gain", juce::NormalisableRange<float> {-12.0f, 12.0f}, 0.0f));
+
+    return layout;
+}
